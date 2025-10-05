f765703f1f2cff0486b18411aa48da903b403ba4
AnhTuan <phanminhanhtuantin@gmail.com>
2025-10-05 22:44:35 +0700

    xong

---
+++ Test.java
@@ -0,0 +1,15 @@
+package THlthdt.Tuan5;
+
+public class Test {
+    public static void main(String[] args) {
+        
+        Sach s2 = new Sach();
+        s2.setMaSach("A01");
+        s2.setTieuDe("Pháp Y");
+        s2.setTacGia("Lưu Huy");
+        s2.setNamXuatBan(2022);
+        s2.setSoLuong(5);
+
+        s2.hienThiThongTin();
+    }
+}
